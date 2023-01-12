43f092201c71fe503f8f18eec0cf8d719a57ce86
LipatovAlexander <lipatov.work@bk.ru>
2023-01-12 18:48:31 +0300

    feat: Add entity framework

---
+++ src/Infrastructure/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Application.Common.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure;
+
+public sealed class ApplicationDbContext : DbContext, IApplicationDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(AssemblyMarker).Assembly);
+    }
+}
---
+++ src/Infrastructure/Configurations/DbContextConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using Application.Common.Interfaces;
+using Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+// ReSharper disable once CheckNamespace
+namespace Microsoft.Extensions.DependencyInjection;
+
+public static class DbContextConfiguration
+{
+    public static IServiceCollection AddDbContext(this IServiceCollection services, IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString("DefaultConnection");
+    
+        services.AddDbContext<ApplicationDbContext>(options =>
+        {
+            options.UseNpgsql(connectionString);
+        });
+
+        services.AddScoped<IApplicationDbContext>(provider => provider.GetRequiredService<ApplicationDbContext>());
+
+        return services;
+    }
+}
